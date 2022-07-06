e8e67e6d7770f249e71e15ebe899430f15cbf14a
EbvidPro <davidebenezer93@gmail.com>
2022-07-06 11:42:11 +0100

    Initial commit

---
+++ components/Layout.js
@@ -0,0 +1,46 @@
+import Link from 'next/link'
+
+export default function Layout({ children }) {
+    return (
+        <div>
+            <header>
+                <nav className='bg-blue-600 px-10 py-5 flex w-full m-0 inset-0'>
+                    <div className='justify-start'>
+                        <h1 className='px-4 text-xl text-white font-bold'>
+                            <a href="/">Marmite</a>
+                        </h1>
+                    </div>
+                    <ul className='flex justify-center text-white items-center m-auto font-semibold'>
+                        <li className='px-6 '>
+                            <a href="#" className='hover:text-yellow-400'>Home</a>
+                        </li>
+                        <li className='px-6 '>
+                            <a href="#" className='hover:text-yellow-400'>About</a>
+                        </li>
+                        <li className='px-6 '>
+                            <a href="#" className='hover:text-yellow-400'>Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div className='text-center p-8'>
+                    <Link href='/'>
+                        <a>
+                            <h1>
+                                <span className='text-lg font-medium'>JUST ADD </span>
+                                <span className='block font-bold text-3xl '>MARMITE</span>
+                            </h1>
+                            <h2 className='font-medium'>SPREAD THE JOY</h2>
+                        </a>
+                    </Link>
+                </div>
+            </header>
+
+            <div>
+                {children}
+            </div>
+            <footer className="flex h-24 w-full self-center justify-center border-t">
+                <p>Copyright @ 2021</p>
+            </footer>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import { createClient } from 'contentful'
+import RecipeCard from '../components/RecipeCard'
+
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  })
+
+  const res = await client.getEntries({ content_type: 'recipe' })
+  return {
+    props: {
+      recipes: res.items
+    }
+  }
+}
+
+const Home = ({ recipes }) => (
+  <div>
+    {console.log({ recipes })}
+    <Head>
+      <title>Marmite Blog</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <main className="px-10 py-2">
+      <h4 className='text-xl font-bold text-left'>Latest Recipes</h4>
+      <div className='p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10'>
+        {/* Cards go here */}
+        {recipes.map(recipe => (
+          <RecipeCard key={recipe.sys.id} recipe={recipe} className='max-w-sm rounded overflow-hidden shadow-lg text-center sm:m-auto' />
+        ))}
+      </div>
+    </main>
+  </div>
+)
+
+export default Home
