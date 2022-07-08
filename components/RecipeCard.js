86ad0bc6221d0d846ffe8167c4f4a9802d4d3458
EbvidPro <davidebenezer93@gmail.com>
2022-07-08 09:05:11 +0100

    Update

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,20 +1,24 @@
+import Image from "next/image"
 import Link from "next/link"
 
 export default function RecipeCard({ recipe }) {
     const { title, slug, cookingTime, thumbnail } = recipe.fields
 
     return (
-        <div>
+        <div className='max-w-sm rounded overflow-hidden shadow-lg text-center sm:m-auto'>
             <div>
-                {/* Image thumbnail*/}
+                <Image src={'https:' + thumbnail.fields.file.url}
+                    width={thumbnail.fields.file.details.image.width}
+                    height={thumbnail.fields.file.details.image.height}
+                />
             </div>
-            <div className=''>
+            <div className='py-4'>
                 <div>
-                    <h4>{title}</h4>
-                    <p>Takes approx {cookingTime} mins to make</p>
+                    <h4 className="text-lg font-bold">{title}</h4>
+                    <p className="italic">Takes approx {cookingTime} mins to make</p>
                 </div>
-                <div>
-                    <Link href={'/recipes/' + slug}><a>Cook this</a></Link>
+                <div className="my-4">
+                    <Link href={'/recipes/' + slug}><a className="text-orange-600 font-semibold hover:text-white hover:p-2 hover:bg-blue-600 rounded">Cook this</a></Link>
                 </div>
             </div>
         </div>
