2af48efb1b7f18251172d436f02b80260d084576
EbvidPro <davidebenezer93@gmail.com>
2022-07-27 20:29:00 +0100

    Incremental Static Regeneration

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -31,7 +31,8 @@ export async function getStaticProps({ params }) {
     })
 
     return {
-        props: { recipe: items[0] }
+        props: { recipe: items[0] },
+        revalidate: 1
     }
 }
 
